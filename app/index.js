cd53a079ffbb276e057d46d67dd1a4b922e671b1
Connor Bode <connor.bode@gmail.com>
2014-12-21 12:20:59 -0700

    initial commit

---
+++ app/index.js
@@ -0,0 +1,90 @@
+var generators = require('yeoman-generator');
+var tpls = {};
+
+module.exports = generators.Base.extend({
+
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+    this.option('coffee');
+  },
+
+  prompting: {
+    packageJson: function () {
+      var done = this.async();
+      this.prompt([
+        {
+          type: 'input',
+          name: 'packageName',
+          message: 'What is the name of the package?'
+        },
+        {
+          type: 'input',
+          name: 'version',
+          message: 'What is the version of the package?'
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'What is the description of the package?'
+        },
+        {
+          type: 'input',
+          name: 'author',
+          message: 'Who is the author?'
+        },
+        {
+          type: 'input',
+          name: 'license',
+          message: 'What license is governing the project?'
+        }
+      ], function (answers) {
+        tpls.packageJson = answers;
+        done();
+      })
+    },
+
+    database: function () {
+      var done = this.async();
+      this.prompt({
+        type: 'list',
+        message: 'Do you want to add a data-source?',
+        choices: [
+          'yes',
+          'no'
+        ],
+        name: 'database'
+      }, function (answers) {
+        if (answers.database === 'yes') {
+          this.prompt({
+            type: 'list',
+            message: 'Which data-source would you like to add?',
+            choices: [
+              'sequelize'
+            ],
+            name: 'database'
+          }, function (answers) {
+            done();
+          });
+        }
+      }.bind(this));
+    }
+  },
+
+  writing: {
+    routes: function () {
+      this.fs.copyTpl(
+        this.templatePath('app/routes.js'),
+        this.destinationPath('app/routes.js'),
+        {}
+      );
+    },
+
+    package: function () {
+      this.fs.copyTpl(
+        this.templatePath('package.json'),
+        this.destinationPath('package.json'),
+        tpls.packageJson
+      );
+    }
+  }
+});
---
+++ app/templates/app/routes.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+
+//RouteInsertReference
+
+app.listen(3000);
